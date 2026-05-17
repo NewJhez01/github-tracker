ed48a66762cfed4dab835fcdd66d68ee10970848
Owen Harper-Jones <owen.harperjones@gmail.com>
2026-05-17 18:15:16 +0200

    handle edge case for empty result for day

--- internal/domain/command/generateReport.go
+++ internal/domain/command/generateReport.go
@@ -9,13 +9,16 @@ import (
 )
 
 func GenerateReport(b []byte, s string) {
+	if string(b) == "[]" {
+		fmt.Println("No data for day")
+	}
 	c, err := infrastructure.ParseJson(b)
 	if err != nil {
 		fmt.Println("parser func failed")
 	}
 	r := formatter.CreateReport(c, s)
 	fmt.Println(r)
-	repo.Get()
 	// 3 call repository func to cache the markdown
+	repo.Get()
 	// 4 dispatch the message for the message handler to async handle it
 }
