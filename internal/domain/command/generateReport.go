86e244d16c4d0fd9b7490956adfca25f4e3b64c9
Owen Harper-Jones <owen.harperjones@gmail.com>
2026-05-18 18:13:09 +0200

    implement the mail sending via smtp

--- internal/domain/command/generateReport.go
+++ internal/domain/command/generateReport.go
@@ -20,10 +20,8 @@ func GenerateReport(b []byte, s string, since time.Time) {
 		fmt.Println("parser func failed")
 	}
 	r := formatter.CreateReport(c, s)
-	// 3 call repository func to cache the markdown
 	ctx := context.Background()
 	yesterday := since.Format("2006-01-02")
 	repo.Set(ctx, r, yesterday)
-	// 4 dispatch the message for the message handler to async handle it
 	rabbitmq.Send(yesterday)
 }
--- internal/domain/command/sendReport.go
+++ internal/domain/command/sendReport.go
@@ -2,12 +2,15 @@ package command
 
 import (
 	"context"
-	"fmt"
 
+	"NewJhez01/github-tracker/internal/infrastructure/email"
 	"NewJhez01/github-tracker/internal/repo"
 )
 
 func SendReport(b []byte) {
 	ctx := context.Background()
-	fmt.Println(repo.Get(ctx, string(b)))
+	r := (repo.Get(ctx, string(b)))
+	// to do handle secrets
+	c := email.CreateSmtpClient("**************")
+	c.Send("Github activities for today", r)
 }
